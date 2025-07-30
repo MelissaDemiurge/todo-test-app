26e923e2ebaa99f3d0b5dbd7c96e48285528aef7
Melissa_Demiurge <melissademiurgy@gmail.com>
2025-07-30 20:34:42 +0500

    еще более подправил, последняя версия до ajax и фишек

--- todo_app/models.py
+++ todo_app/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
+    title = db.Column(db.String(24), nullable=False, unique=True)
     done = db.Column(db.Boolean, default=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
