bbfbbb6937f0cf8733c1122ca792a7d44a7d8278
Melissa_Demiurge <melissademiurgy@gmail.com>
2025-08-24 22:42:32 +0500

    коррекция3

--- todo_app/models.py
+++ todo_app/models.py
@@ -9,6 +9,7 @@ class Task(db.Model):
     title = db.Column(db.String(24), nullable=False, unique=True)
     done = db.Column(db.Boolean, default=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return f'<Task id={self.id} title="{self.title}" done={self.done}>'
